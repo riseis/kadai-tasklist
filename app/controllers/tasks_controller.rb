751f4d7e60165428c455ac32ca01014668a83d4f
risei.sasaki <risei19980612@icloud.com>
2020-11-18 05:49:46 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,12 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  # before_action :require_user_logged_in
   
   def index
-    @tasks = Task.all
+    # @tasks = Task.all
+
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+
   end
 
   def show
@@ -14,7 +17,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
