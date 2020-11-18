28c46bb54f65e3772756df3df78f1be2bc00b319
risei.sasaki <risei19980612@icloud.com>
2020-11-18 07:45:44 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -53,7 +53,11 @@ class TasksController < ApplicationController
 
   # Strong Parameter
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+    # @task = Task.find(params[:id])
   end
   
   def task_params
